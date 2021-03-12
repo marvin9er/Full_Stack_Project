d839516930a00a2cd9678c6f36ed063ebd754744
Justin Horton <justinhorton125@gmail.com>
2021-03-12 13:59:21 -0600

    Update seeds.rb
    Updated seeds.rb file to include all models. Race and Traits commented out because of weird bug. Language works perfectly, while subraces should work but doesn't actually add to the database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,34 +9,45 @@ def full_url(url)
   "https://dnd5eapi.co#{url}"
 end
 
-races = swapi_fetch('https://dnd5eapi.co/api/races/')
+# races = swapi_fetch('https://dnd5eapi.co/api/races/')
 
-races['results'].each do |race_object|
+# races['results'].each do |race_object|
 
-  race_url = race_object['url']
+#   race_url = race_object['url']
 
-  race = swapi_fetch(full_url(race_url))
+#   race = swapi_fetch(full_url(race_url))
 
-  subrace = ''
+#   subrace = ''
 
-  if race['subraces'].any?
+#   if race['subraces'].any?
 
-    race['subraces'].each do |race_subrace|
+#     race['subraces'].each do |race_subrace|
 
-      if !subrace.eql? ''
-        subrace += ', '
-      end
-      subrace += race_subrace['name']
+#       if !subrace.eql? ''
+#         subrace += ', '
+#       end
+#       subrace += race_subrace['name']
 
-    end
+#     end
 
-  end
+#   end
 
-  Race.create(index:race['index'], name:race['name'], speed:race['speed'], alignment:race['alignment'], age:race['age'],
-     size:race['size'], size_desc:race['size_desc'], language_desc:race['language_desc'], subraces:subrace, url:race['url'])
+#   # t.string "index"
+#   # t.string "name"
+#   # t.integer "speed"
+#   # t.string "alignment"
+#   # t.string "age"
+#   # t.string "size"
+#   # t.string "size_desc"
+#   # t.string "language_desc"
+#   # t.string "subraces"
+#   # t.string "url"
+
+#   Race.create(index:race['index'], name:race['name'], speed:race['speed'], alignment:race['alignment'], age:race['age'],
+#      size:race['size'], size_desc:race['size_desc'], language_desc:race['language_desc'], subraces:subrace, url:race['url'])
 
 
-end
+# end
 
 
 subraces = swapi_fetch('https://dnd5eapi.co/api/subraces/')
@@ -47,9 +58,62 @@ subraces['results'].each do |subrace_object|
 
   subrace = swapi_fetch(full_url(subrace_url))
 
+  # t.string "index"
+  # t.string "name"
+  # t.string "race"
+  # t.string "desc"
+  # t.string "url"
 
-
+  Subrace.create(index:subrace['index'], name:subrace['name'], race:subrace['race'], desc:subrace['desc'], url:subrace['url'])
 
 end
 
+# languages = swapi_fetch('https://dnd5eapi.co/api/languages')
 
+# languages['results'].each do |language_object|
+#   language_url = language_object['url']
+
+#   language = swapi_fetch(full_url(language_url))
+
+#   typical_speakers = ''
+
+#   language['typical_speakers'].each do |speaker|
+#     if !typical_speakers.eql? ''
+#       typical_speakers += ', '
+#     end
+#     typical_speakers += speaker
+
+#     puts typical_speakers
+#   end
+
+
+
+  # t.string "index"
+  # t.string "name"
+  # t.string "type"
+  # t.string "typical_speakers"
+  # t.string "script"
+  # t.string "url"
+
+#   Language.create(index:language['index'], name:language['name'], lang_type:language['type'], typical_speakers:typical_speakers, script:language['script'], url:language['url'])
+
+# end
+
+
+# traits = swapi_fetch('https://dnd5eapi.co/api/traits')
+
+# traits['results'].each do |trait_object|
+#   trait_url = trait_object['url']
+
+#   trait = swapi_fetch(full_url(trait_url))
+
+#   # t.string "index"
+#   # t.string "races"
+#   # t.string "subraces"
+#   # t.string "name"
+#   # t.string "desc"
+#   # t.string "url"
+
+#   Trait.create(index:trait['index'], races:trait['races'], subraces:trait['subraces'], name:trait['name'], desc:trait['desc'], url:trait['url'])
+
+# end
